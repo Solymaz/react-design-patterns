7e80b7e86c57ca74bd6800e8fc5de7378f0c1db1
Solmaz <solmaz.mousavie@gmail.com>
2022-10-05 11:07:46 +0200

    Split screen

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: "green" }}>Left</h1>;
+};
+const RightHandComponent = () => {
+  return <p style={{ backgroundColor: "red" }}>Right</p>;
+};
+
+function App() {
+  return (
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent />
+      <RightHandComponent />
+    </SplitScreen>
+  );
+}
+
+export default App;
