8ce0a91b346d2ca82e67e93d93f0ac552571920d
Solmaz <solmaz.mousavie@gmail.com>
2022-10-07 11:59:58 +0200

    userLoader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserInfo } from './UserInfo';
+import { UserLoader } from "./UserLoader";
+import { UserInfo } from "./UserInfo";
 
 function App() {
-	return (
-		<CurrentUserLoader>
-			<UserInfo />
-		</CurrentUserLoader>
-	);
+  return (
+    <UserLoader userId="234">
+      <UserInfo />
+    </UserLoader>
+  );
 }
 
 export default App;
