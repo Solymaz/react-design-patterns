d657876f72f8ecd5cba26fffc84b0a6287bdbd8f
Solmaz <solmaz.mousavie@gmail.com>
2022-10-11 17:19:03 +0200

    custom hook ex4

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,20 +1,31 @@
-import { useResource } from './useResource';
-import { useUser } from './useUser';
+import { useDataSource } from "./useDataSource";
+import axios from "axios";
+
+const serverResource = (resourceUrl) => async () => {
+  const response = await axios.get(resourceUrl);
+  return response.data;
+};
 
 export const UserInfo = ({ userId }) => {
-	const user = useResource(`/users/${userId}`);
+  const user = useDataSource(serverResource(`/users/${userId}`));
 
-	const { name, age, hairColor, hobbies } = user || {};
+  // const user = useResource(`/users/${userId}`);
 
-	return user ? (
-		<>
-		<h3>{name}</h3>
-		<p>Age: {age} years</p>
-		<p>Hair Color: {hairColor}</p>
-		<h3>Hobbies:</h3>
-		<ul>
-			{hobbies.map(hobby => <li key={hobby}>{hobby}</li>)}
-		</ul>
-		</>
-	) : <p>Loading...</p>;
-}
+  const { name, age, hairColor, hobbies } = user || {};
+
+  return user ? (
+    <>
+      <h3>{name}</h3>
+      <p>Age: {age} years</p>
+      <p>Hair Color: {hairColor}</p>
+      <h3>Hobbies:</h3>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
+    </>
+  ) : (
+    <p>Loading...</p>
+  );
+};
