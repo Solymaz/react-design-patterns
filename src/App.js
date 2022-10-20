84630aec2723a0c9d1cca8938930f06b98b04846
Solmaz <solmaz.mousavie@gmail.com>
2022-10-20 14:28:09 +0200

    button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { RecursiveComponent } from "./RecursiveComponent";
-
+import { BigSuccessButton, DangerButton } from "./composition";
 const nestedObject = {
-	a: 1,
-	b: {
-		b1: 4,
-		b2: {
-			b23: 'Hello',
-		},
-		b3: {
-			b31: {
-				message: 'Hi',
-			},
-			b32: {
-				message: 'Hi',
-			}
-		}
-	},
-	c: {
-		c1: 2,
-		c2: 3,
-	}
-}
+  a: 1,
+  b: {
+    b1: 4,
+    b2: {
+      b23: "Hello",
+    },
+    b3: {
+      b31: {
+        message: "Hi",
+      },
+      b32: {
+        message: "Hi",
+      },
+    },
+  },
+  c: {
+    c1: 2,
+    c2: 3,
+  },
+};
 
 function App() {
-	return (
-		<RecursiveComponent data={nestedObject} />
-	);
+  return (
+    <>
+      <DangerButton text="No" />
+      <BigSuccessButton text="Yes" />
+    </>
+  );
 }
 
 export default App;
